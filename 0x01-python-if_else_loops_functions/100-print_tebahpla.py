b737ad4d405a9daaba64de6d1e009e304948a95b
fortune-07 <fortuneniguel@gmail.com>
2021-04-29 17:34:13 +0000

    if else loops

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a') - 1, -2):
-    print("{:c}{:s}".format(c, chr(c - 33)), end="")
+i = 122
+while i >= 97:
+    flag = 0
+    if i % 2 != 0:
+        i = i - 32
+        flag = 1
+    print("{:s}".format(chr(i)), end="")
+    if flag == 1:
+        i = i + 32
+    i = i - 1
