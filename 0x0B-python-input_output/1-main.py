fcd5ed1beee266e3effe0560807f4203a7d0724e
fortune-07 <fortuneniguel@gmail.com>
2021-06-01 20:05:17 +0000

    input_output

---
+++ 0x0B-python-input_output/1-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+write_file = __import__('1-write_file').write_file
+
+nb_characters = write_file("my_first_file.txt", "Holberton School is so cool!\n")
+print(nb_characters)
+
+guillaume@ubuntu:~/0x0B$ ./1-main.py
