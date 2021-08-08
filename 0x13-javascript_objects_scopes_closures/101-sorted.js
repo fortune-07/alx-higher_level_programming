e8dafe494ac2b71c921191a0f2702bd85670528d
fortune-07 <fortuneniguel@gmail.com>
2021-08-08 14:16:04 +0000

    corrections

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,12 +2,10 @@
 // Script imports a dictionary of occurrences by user id
 // and computes a dictionary of user ids by occurrence.
 
-const { dict } = require('./101-data.js');
-const Dictn = {};
-for (const N in dict) {
-    if (Dictn[dict[N]] === undefined) {
-	Dictn[dict[N]] = [];
-    }
-    Dictn[dict[N]].push(N);
-}
-console.log(Dictn);
+const { dict } = require('./101-data');
+
+const myValue = Object.entries(dict).reduce((acc, [key, value]) => {
+  acc[value] = acc[value] ? [...acc[value], key] : [key];
+  return acc;
+}, {});
+console.log(myValue);
