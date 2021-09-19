7ea11d6aec44228e807c262a5a4b2486491a37e4
fortune-07 <fortuneniguel@gmail.com>
2021-09-19 03:25:13 -0700

    updated task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
-
 class Square:
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
     """
-    class Square with private instance attribute size
-    """
+
     def __init__(self, size):
+        """Initializes the data."""
         self.__size = 
