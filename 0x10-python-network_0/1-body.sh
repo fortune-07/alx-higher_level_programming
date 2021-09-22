01db602bbb860467f9b6edeb29d007955ea3ec47
fortune-07 <fortuneniguel@gmil.com>
2021-09-22 07:24:15 +0000

    updated repo

--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Script takes URL, sends GET request and displays body of response
-curl -sL "$1"
--- 0x10-python-network_0/5-post_params.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Script sends POST to URL
-curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
