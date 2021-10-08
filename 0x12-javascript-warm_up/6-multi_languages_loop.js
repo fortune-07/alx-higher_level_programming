27ac93091fa7b796471ef59b5fe7a91e6bc88626
fortune-07 <fortuneniguel@gmail.com>
2021-10-08 21:30:19 +0000

    langauages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-// prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
-
-'use strict';
-const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const arg in args) {
-  console.log(args[arg]);
+const s = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(s[i]);
 }
